f232641ef77c931cd22c064c07793c4a30d1f0b4
Michael Vangelovski <michael.vangelovski@hotmail.com>
2026-05-08 11:56:19 +1000

    Rebuild

--- frontend/src/pages/Risk/Risk.tsx
+++ frontend/src/pages/Risk/Risk.tsx
@@ -909,11 +909,12 @@ export function Risk() {
               mb="md"
               required
               data={[
-                { value: '1', label: '1 - Very High' },
+                { value: '1', label: '1 - Extreme' },
                 { value: '2', label: '2 - High' },
                 { value: '3', label: '3 - Medium' },
-                { value: '4', label: '4 - Low' },
-                { value: '5', label: '5 - Very Low' },
+                { value: '3', label: '4 - Medium' },
+                { value: '4', label: '5 - Low' },
+                { value: '5', label: '6 - Very Low' },
               ]}
             />
             <Select
@@ -923,11 +924,12 @@ export function Risk() {
               mb="md"
               required
               data={[
-                { value: '1', label: '1 - Very High' },
+                { value: '1', label: '1 - Extreme' },
                 { value: '2', label: '2 - High' },
                 { value: '3', label: '3 - Medium' },
-                { value: '4', label: '4 - Low' },
-                { value: '5', label: '5 - Very Low' },
+                { value: '3', label: '4 - Medium' },
+                { value: '4', label: '5 - Low' },
+                { value: '5', label: '6 - Very Low' },
               ]}
             />
           </Group>
