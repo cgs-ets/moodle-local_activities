ff9d7bc3f9c0e2c7f84cc714969a374ddbc7625e
Michael Vangelovski <michael.vangelovski@hotmail.com>
2026-05-12 14:51:15 +1000

    Rebuild

--- frontend/src/pages/Assessment/Assessment.tsx
+++ frontend/src/pages/Assessment/Assessment.tsx
@@ -338,7 +338,7 @@ export function Assessment() {
 
   useEffect(() => {
     // When the selected module changes, update the assessment name.
-    if (formData.module && formData.module.label && !formData.name) {
+    if (formData.module && formData.module.label) {
       updateField('name', formData.module.label)
     }
   }, [formData.module])
